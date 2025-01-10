cf6684b95668eb7e3a3e566a0089d1f5d12b0009
dario <darioanwar3@gmail.com>
2025-01-10 12:28:35 +0100

    feat : create new page login

---
+++ app/login/page.jsx
@@ -0,0 +1,25 @@
+import Form from "next/form"
+import Input from "../components/Input"
+import SecondaryButton from "../components/SecondaryButton"
+
+export default (params) => {
+  return (
+   
+    <Form className="form" action="/search">
+      <h2>Sign in</h2>
+   
+     <Input
+      type="email"
+      placeholder="Enter your email"
+      label="email"
+      />
+      <Input
+      type="password"
+      placeholder="Enter your password"
+      label="Enter your password"
+      />
+      <SecondaryButton content="Sign in"/>
+    </Form>
+    
+  )
+}
