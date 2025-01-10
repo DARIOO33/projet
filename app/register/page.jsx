626051bc8b9468e4dc60222d33312a0667595ec8
dario <darioanwar3@gmail.com>
2025-01-10 21:13:03 +0100

    adding register pages

---
+++ app/register/page.jsx
@@ -0,0 +1,36 @@
+import Form from "next/form"
+import Input from "../components/Input"
+import SecondaryButton from "../components/SecondaryButton"
+
+
+export default (params) => {
+  return (
+   
+    <Form className="form" action="/search">
+      <h2>Sign up</h2>
+      <Input
+      type="text"
+      placeholder="Enter your name"
+      label="fullname"
+      />
+     <Input
+      type="email"
+      placeholder="Enter your email"
+      label="email"
+      />
+      <Input
+      type="password"
+      placeholder="Enter your password"
+      label="Enter your password"
+      />
+       <Input
+      type="password"
+      placeholder="Confirm your password"
+      label="Confirm your password"
+      />
+<SecondaryButton content="Sign up"/>
+
+</Form>
+    
+  )
+}
